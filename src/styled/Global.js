07f9656fe488c2aa626c3a0227b2d5d26918d790
Kowacka Malgorzata <kowackag@gmail.com>
2022-03-18 06:50:24 +0100

    Change Global.js - the way to add fonts

--- src/styled/Global.js
+++ src/styled/Global.js
@@ -1,4 +1,10 @@
 import {createGlobalStyle} from 'styled-components';
+import robotoRegularWoff from "./../fonts/roboto-regular-webfont.woff";
+import robotoRegularWoff2 from "./../fonts/roboto-regular-webfont.woff2";
+import robotoBoldWoff from "./../fonts/roboto-bold-webfont.woff";
+import robotoBoldWoff2 from "./../fonts/roboto-bold-webfont.woff2";
+import robotoItalicWoff2 from "./../fonts/roboto-italic-webfont.woff2";
+import robotoItalicWoff from "./../fonts/roboto-italic-webfont.woff";
 
 const GlobbalStyle = createGlobalStyle`
     @font-face {
@@ -6,25 +12,23 @@ const GlobbalStyle = createGlobalStyle`
         font-style: regular;
         font-weight: 400;
         src:
-        url("./../fonts/roboto-regular-webfont.woff2") format('woff2'),
-        url('./../fonts/roboto-regular-webfont.woff') format('woff');
+        url(${robotoRegularWoff2}) format('woff2'),
+        url(${robotoRegularWoff}) format('woff');
     }
-
     @font-face {
         font-family: "Roboto";
         font-weight: 700;
         src:
-        url('./../fonts/roboto-bold-webfont.woff2') format('woff2'),
-        url('./../fonts/roboto-bold-webfont.woff') format('woff');
+        url(${robotoBoldWoff2}) format('woff2'),
+        url(${robotoBoldWoff}) format('woff');
     }
-
     @font-face {
         font-family: "Roboto";
         font-style: italic;
         font-weight: 400;
         src:
-        url('./../fonts/roboto-italic-webfont.woff2') format('woff2'),
-        url('./../fonts/roboto-italic-webfont.woff') format('woff');
+        url(${robotoItalicWoff2}) format('woff2'),
+        url(${robotoItalicWoff}) format('woff');
     }
 
     html {
